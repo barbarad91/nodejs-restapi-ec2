4d2bb6fb24b475dfeffd9a578ee4de6cf7ca9638
barbarad91 <barbarad91dev@gmail.com>
2024-05-31 12:48:49 +0200

    Update dotenv

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import dotenv from 'dotenv'
+import 'dotenv/config'
 
 import express from 'express'
 import cors from 'cors'
@@ -6,7 +6,6 @@ import cors from 'cors'
 import { connectDB } from './config/db.js'
 import userRoutes from './routes/users.js'
 import { errorHandler } from './middlewares/error.js'
-dotenv.config()
 
 // Connect to DB
 connectDB()
